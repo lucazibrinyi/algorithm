b534f1a794c2f4159a100e66fe4325ae04a37943
Luca Zibrinyi <zvmluci@gmail.com>
2021-03-24 15:41:28 +0100

    Odd numbers formated

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -5,12 +5,12 @@ import java.util.List;
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        Integer sum=0;
-        for(Integer number : numbers) {
-            if(number%2==1 && number>0){
-                sum+=number;
-            }else if(number%2==-1 && number<0){
-                sum+=number;
+        Integer sum = 0;
+        for (Integer number : numbers) {
+            if (number % 2 == 1 && number > 0) {
+                sum += number;
+            } else if (number % 2 == -1 && number < 0) {
+                sum += number;
             }
         }
         return sum;
